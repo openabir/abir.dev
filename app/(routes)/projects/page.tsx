31ae735b7c49bf20e226f88c5c0f07e092531c7f
openabir <heyyabir@gmail.com>
2025-08-01 02:14:13 +0530

    Homepage

--- app/(routes)/projects/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function projects() {
-  return (
-    <h1 className="text-6xl font-bold underline">
-      My Projects??? 👍
-    </h1>
-  );
-}
--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,45 @@
+import React from "react";
+import Link from "next/link";
+import { Navbar } from "@/components/Navbar";
+import MapComponent from "@/components/MapComponent";
+import Profile from "@/components/Profile";
+import AboutText from "@/components/About";
+import { DockComponent } from "@/components/DockComponent";
+import TechGrid from "@/components/TechGrid";
+import Footer from "@/components/Footer";
 
-export default function Home() {
+export default function About() {
   return (
     <>
-      <div className="bg-gray-100 flex items-center justify-center min-h-[90vh] dark:bg-gray-900">
-        <div className="text-center">
-          <h1 className="pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-black to-gray-300/80 bg-clip-text text-center text-9xl font-semibold leading-none text-transparent dark:from-white dark:to-slate-900/10">Hello World</h1>
-          <div>
-            <br />
+      <Navbar />
+
+      <div>
+        <section className="w-full h-full rounded-2xl bg-background">
+          <div className="group relative h-48">
+            <MapComponent />
           </div>
-          <h2 className="text-4xl font-semibold mb-2">I&apos;m <span className="text-transparent text-stroke">ABIR BISWAS.</span></h2>
-          <p className="text-2xl mb-4">a Web Developer</p>
+          <div className="opacity-100 transform-none">
+            <Profile />
+          </div>
+          <div className=" opacity-100 bg-transparent">
+            <AboutText />
+          </div>
+          <div className="opacity-100 transform-none">
+            <TechGrid />
+            <Link
+              href="/tech-stack"
+              className="text-sm text-blue-500 hover:underline hover:text-blue-700 transition-colors duration-200"
+            >
+              more...
+            </Link>
+          </div>
+
+          <div className="h-100px">
+            <Footer />
+          </div>
+        </section>
+        <div className="fixed bottom-5 left-0 right-0 flex justify-center z-999">
+          <DockComponent />
         </div>
       </div>
     </>
